5613b840b19f45d23b2a9cf2cbba8568e2784aba
Federico Voges <federico@hashicorp.com>
2020-01-30 14:21:29 +0000

    add remote backed config

--- config.tf
+++ config.tf
@@ -5,3 +5,14 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
+
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "hc-implementation-services"
+
+    workspaces {
+      name = "boats-terraform-basic-infra"
+    }
+  }
+}
