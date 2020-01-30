ce891ce15e91bee210c1ac009b4437b614d34b37
Federico Voges <federico@hashicorp.com>
2020-01-30 12:00:59 +0000

    initial commit

---
+++ data.tf
@@ -0,0 +1,23 @@
+data "aws_ami" "ubuntu" {
+    most_recent = true
+
+    filter {
+        name   = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-*-18.04-amd64-server-*"]
+    }
+
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+
+    owners = ["099720109477"] # Canonical
+}
+
+data "template_file" "bastion_user_data" {
+  template = file("${path.module}/templates/bastion_user_data.sh")
+}
+
+data "aws_route53_zone" "selected" {
+  name         = var.route53_zone
+}
---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "aws_region" {
+  type        = string
+  description = "AWS region to deploy to"
+  default     = "eu-west-2"
+}
+
+variable "project" {
+  type        = string
+  description = "Friendly name for this deployment used for tagging and naming resources"
+}
+
+variable "bastion_key_pair" {
+  type        = string
+  description = "Existing SSH key pair for bastion host"
+}
+
+variable "bastion_ingress_cidr_allow" {
+  type        = list(string)
+  description = "List of CIDR's to allow ingress to bastion EC2 instance security group"
+}
+
+variable "route53_zone" {
+  type        = string
+  description = "Route53 hosted DNS domain used to setup DNS records"
+}
