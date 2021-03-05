8c207cc3170ea6b080c4d42db1b809a15f6b80f7
Federico Voges <federico@hashicorp.com>
2021-03-05 18:22:36 +0000

    add outputs

---
+++ outputs.tf
@@ -0,0 +1,15 @@
+output "private_subnets" {
+  value = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+  value = module.vpc.private_subnets
+}
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "bastion" {
+  value = aws_route53_record.bastion[0].name
+}
