5998d60c73b251fae4887738645ee662e86a3e3a
Federico Voges <federico@hashicorp.com>
2021-03-05 14:50:50 +0000

    comment out TFC config

--- config.tf
+++ config.tf
@@ -6,13 +6,13 @@ provider "aws" {
   region = var.aws_region
 }
 
-terraform {
-  backend "remote" {
-    hostname = "app.terraform.io"
-    organization = "hc-implementation-services"
+# terraform {
+#   backend "remote" {
+#     hostname = "app.terraform.io"
+#     organization = "hc-implementation-services"
 
-    workspaces {
-      name = "boats-terraform-basic-infra"
-    }
-  }
-}
+#     workspaces {
+#       name = "boats-terraform-basic-infra"
+#     }
+#   }
+# }
